9534213c7258723a5a648943bf6d7ec9d8ad4762
ectuser <m.ectuser@gmail.com>
2020-08-25 04:03:01 +0700

    Added feature to rewrite default options but keep the last part of them in the object

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import {ISalesNavigatorConverterOptions, salesNavigatorConverterDefaultOptions} from "./salesNavigatorConverterOptions";
 import { RegexMatchError } from "./RegexMatchError";
 function convertMultipleSalesNavigatorLinks(salesNavigatorLinks: string[], options: ISalesNavigatorConverterOptions = salesNavigatorConverterDefaultOptions) : string[]{
+    options = {...salesNavigatorConverterDefaultOptions, ...options};
+    console.log(options);
     let links = [];
     for (let link of salesNavigatorLinks){
         let salesNavigatorLink = "";
@@ -32,7 +34,7 @@ function convertMultipleSalesNavigatorLinks(salesNavigatorLinks: string[], optio
 }
 function convertSalesNavigatorLink(link: string){
     // template - https://www.linkedin.com/sales/people/ACwAAADesZ8BkzsAiMtKrLgIbVOh23MGN9mPUXM,NAME_SEARCH,OCaE?_ntb=inVK0P7mS3qRTYjpxGalTg%3D%3D
-    let re = /.*linkedin.com\/sales\/people\/.+,/;
+    let re = /.*linkedin.com\/sales\/people\/.+/;
     if (!re.test(link)){
         throw new RegexMatchError("Link does not fit sales nav link template");
     }
@@ -42,4 +44,4 @@ function convertSalesNavigatorLink(link: string){
 
 const links = ["https://www.linkedin.com/sale/people/ACwAAACAlGcB3ekSocf6RLAcBY6T8cxs85iszb0,NAME_SEARCH,1eb2?_ntb=xfImurByQtaDeDyrb9pJmg%3D%3D", "https://www.linkedin.com/sales/people/ACwAAADesZ8BkzsAiMtKrLgIbVOh23MGN9mPUXM,NAME_SEARCH,OCaE?_ntb=inVK0P7mS3qRTYjpxGalTg%3D%3D"];
 
-console.log(convertMultipleSalesNavigatorLinks(links));
+console.log(convertMultipleSalesNavigatorLinks(links, {skipInvalidURLs: "-1"}));
--- src/salesNavigatorConverterOptions.ts
+++ src/salesNavigatorConverterOptions.ts
@@ -1,6 +1,6 @@
 export interface ISalesNavigatorConverterOptions{
-    strictMode: boolean; // if true, main function throws an error if there's a non-sales-navigator-url type
-    skipInvalidURLs: string | boolean; // if true - invalid urls will be skipped, false - replace them with -1, any string - replace them with this string
+    strictMode?: boolean; // if true, main function throws an error if there's a non-sales-navigator-url type
+    skipInvalidURLs?: string | boolean; // if true - invalid urls will be skipped, false - replace them with -1, any string - replace them with this string
 };
 export const salesNavigatorConverterDefaultOptions : ISalesNavigatorConverterOptions = {
     strictMode: false,
