d07296c281f14b9c4e27c0e5446304a44bf3dd5f
ectuser <m.ectuser@gmail.com>
2020-08-24 23:48:09 +0700

    Added basic usage

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world")
+function convertMultipleSalesNavigatorLinks(salesNavigatorLinks: string[]) : string[]{
+    return salesNavigatorLinks.map((item: string) => {
+        return convertSalesNavigatorLink(item);
+    })
+}
+function convertSalesNavigatorLink(link: string){
+    return link.replace("sales/people", "in").split(",")[0];
+}
+
+const links = ["https://www.linkedin.com/sales/people/ACwAAACAlGcB3ekSocf6RLAcBY6T8cxs85iszb0,NAME_SEARCH,1eb2?_ntb=xfImurByQtaDeDyrb9pJmg%3D%3D", "https://www.linkedin.com/sales/people/ACwAAADesZ8BkzsAiMtKrLgIbVOh23MGN9mPUXM,NAME_SEARCH,OCaE?_ntb=inVK0P7mS3qRTYjpxGalTg%3D%3D"];
+
+console.log(convertMultipleSalesNavigatorLinks(links));
