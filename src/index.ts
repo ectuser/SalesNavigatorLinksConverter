dc5a006ee0d9e082be48accacda34d3e0c9de23e
ectuser <m.ectuser@gmail.com>
2020-08-26 21:09:05 +0700

    Added feature to get profile id from linkedin and sales navigator link

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {ISalesNavigatorConverterOptions, salesNavigatorConverterDefaultOptions} from "./salesNavigatorConverterOptions";
 import { RegexMatchError } from "./RegexMatchError";
+import { getMultipleLinkedinIds } from "./linkedinIdGetters";
 function convertMultipleSalesNavigatorLinks(salesNavigatorLinks: string[], options: ISalesNavigatorConverterOptions = salesNavigatorConverterDefaultOptions) : string[]{
     options = {...salesNavigatorConverterDefaultOptions, ...options};
     console.log(options);
@@ -9,11 +10,7 @@ function convertMultipleSalesNavigatorLinks(salesNavigatorLinks: string[], optio
         try {
             salesNavigatorLink = convertSalesNavigatorLink(link);
         } catch (error) {
-            if (error instanceof RegexMatchError && options.strictMode){
-                console.log(error.message);
-                throw error;
-            }
-            else if (error instanceof RegexMatchError && !options.strictMode){
+            if (error instanceof RegexMatchError && !options.strictMode){
                 if (options.skipInvalidURLs === true){
                     continue;
                 }
@@ -33,7 +30,7 @@ function convertMultipleSalesNavigatorLinks(salesNavigatorLinks: string[], optio
     return links;
 }
 function convertSalesNavigatorLink(link: string){
-    // template - https://www.linkedin.com/sales/people/ACwAAADesZ8BkzsAiMtKrLgIbVOh23MGN9mPUXM,NAME_SEARCH,OCaE?_ntb=inVK0P7mS3qRTYjpxGalTg%3D%3D
+    // example - https://www.linkedin.com/sales/people/ACwAAADesZ8BkzsAiMtKrLgIbVOh23MGN9mPUXM,NAME_SEARCH,OCaE?_ntb=inVK0P7mS3qRTYjpxGalTg%3D%3D
     let re = /.*linkedin.com\/sales\/people\/.+/;
     if (!re.test(link)){
         throw new RegexMatchError("Link does not fit sales nav link template");
@@ -42,6 +39,8 @@ function convertSalesNavigatorLink(link: string){
     return link.replace("sales/people", "in").split(",")[0];
 }
 
-const links = ["https://www.linkedin.com/sale/people/ACwAAACAlGcB3ekSocf6RLAcBY6T8cxs85iszb0,NAME_SEARCH,1eb2?_ntb=xfImurByQtaDeDyrb9pJmg%3D%3D", "https://www.linkedin.com/sales/people/ACwAAADesZ8BkzsAiMtKrLgIbVOh23MGN9mPUXM,NAME_SEARCH,OCaE?_ntb=inVK0P7mS3qRTYjpxGalTg%3D%3D"];
+const links = ["https://www.linkedin.com/sale/people/ACwAAACAlGcB3ekSocf6RLAcBY6T8cxs85iszb0,NAME_SEARCH,1eb2?_ntb=xfImurByQtaDeDyrb9pJmg%3D%3D", 
+"https://www.linkedin.com/sales/people/ACwAAADesZ8BkzsAiMtKrLgIbVOh23MGN9mPUXM,NAME_SEARCH,OCaE?_ntb=inVK0P7mS3qRTYjpxGalTg%3D%3D",
+"https://www.linkedin.com/in/melindagates/"];
 
-console.log(convertMultipleSalesNavigatorLinks(links, {skipInvalidURLs: "-1"}));
+console.log(convertMultipleSalesNavigatorLinks(links, {skipInvalidURLs: true}));
--- src/linkedinIdGetters.ts
+++ src/linkedinIdGetters.ts
@@ -1,12 +1,32 @@
 import {ISalesNavigatorConverterOptions, salesNavigatorConverterDefaultOptions} from "./salesNavigatorConverterOptions"
 import { RegexMatchError } from "./RegexMatchError";
-function getMultipleLinkedinIds(links: string[], options: ISalesNavigatorConverterOptions = salesNavigatorConverterDefaultOptions): string[]{
+export function getMultipleLinkedinIds(links: string[], options: ISalesNavigatorConverterOptions = salesNavigatorConverterDefaultOptions): string[]{
     options = {...salesNavigatorConverterDefaultOptions, ...options};
-    let ids  = [];
+    let ids = [];
 
     for (let link of links){
-
+        let newId = "";
+        try {
+            newId = getLinkedinId(link);   
+        } catch (error) {
+            if (error instanceof RegexMatchError && !options.strictMode){
+                if (options.skipInvalidURLs === true){
+                    continue;
+                }
+                else if (!options.skipInvalidURLs){
+                    newId = "-1";
+                }
+                else if (options.skipInvalidURLs){
+                    newId = options.skipInvalidURLs;
+                }
+            }
+            else{
+                throw error;
+            }
+        }
+        ids.push(newId);
     }
+    return ids;
 }
 
 function getLinkedinId(link: string){
@@ -14,7 +34,7 @@ function getLinkedinId(link: string){
     let salesNavRegex = /.*linkedin.com\/sales\/people\/.+/;
     let linkedinRegex = /.*linkedin.com\/in\/.+/;
     if (salesNavRegex.test(link)){
-        
+        return getLinkedinIdSalesNavLink(link);
     }
     else if (linkedinRegex.test(link)){
         return getLinkedinIdLinkedinLink(link);
@@ -29,5 +49,13 @@ function getLinkedinIdLinkedinLink(link: string): string{
     let sFirst = link.split("?")[0];
 	let sSecond = sFirst.replace("https://www.linkedin.com/in/", "");
 	let sID = sSecond.split("/")[0];
+	return sID;
+}
+
+function getLinkedinIdSalesNavLink(link: string): string{
+    // example - https://www.linkedin.com/sales/people/ACwAAADesZ8BkzsAiMtKrLgIbVOh23MGN9mPUXM,NAME_SEARCH,OCaE?_ntb=inVK0P7mS3qRTYjpxGalTg%3D%3D
+    let sFirst = link.split("?")[0];
+	let sSecond = sFirst.replace("https://www.linkedin.com/sales/people/", "");
+	let sID = sSecond.split(",")[0];
 	return sID;
 }
